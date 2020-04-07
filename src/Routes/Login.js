77b5da5249b422467b0d010fc629c402492d9197
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-07 10:06:22 +0530

    added login page

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Base from "../Core/Base";
 
 function Login() {
-  return <div></div>;
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-dark">Email</label>
+              <input
+                type="email"
+                placeholder="Email"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-dark">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="form-control"
+              />
+            </div>
+            <button className="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Login" description="Login into the app here !">
+      {signInForm()}
+    </Base>
+  );
 }
 
 export default Login;
